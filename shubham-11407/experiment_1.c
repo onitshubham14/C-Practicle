3a0ea21e39d48a61e8a6ea90c8520f6b4f8cb983
shubham barnwal <shubhambarnwal046@gmail.com>
2026-01-03 16:23:46 +0530

    collage-c-practicles
    this is an assignment submission of my university for c language.

---
+++ shubham-11407/experiment_1.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main() {
+    printf("Hello, World! This is my first C program done in my collage.\n");
+    return 0;
+}
---
+++ shubham-11407/experiment_2.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main() {
+    printf("Name: Shubham barnwal\n");
+    printf("Age: 18\n");
+    return 0;
+}
